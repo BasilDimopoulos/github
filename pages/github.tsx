5d598d3178028479fb0360220e8b58f2a8c0fc32
Basil Dimopoulos <basil.dimopoulos@gmail.com>
2022-11-19 12:46:52 +1030

    feat: added repository details

--- pages/github.tsx
+++ pages/github.tsx
@@ -1,9 +1,11 @@
 import NavBar from "../components/NavBar";
+import RepoDetails from "../components/RepoDetails";
 
 export default function Home() {
     return (
         <div className="text-gray-900 text-sm">
             <NavBar></NavBar>
+            <RepoDetails></RepoDetails>
         </div>
     )
 }
